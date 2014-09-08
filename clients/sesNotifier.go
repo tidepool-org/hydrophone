faf6400c35a6f713e27124f42daa72fe691f51c9
Jamie <jamie@tidepool.org>
2014-09-09 11:37:31 +1200

    updates to config and see to send templates emails

--- clients/sesNotifier.go
+++ clients/sesNotifier.go
@@ -14,18 +14,15 @@ import (
 	"time"
 )
 
-const (
-	endpoint = "https://email.us-east-1.amazonaws.com"
-)
-
 type (
 	SesNotifier struct {
 		Config *SesNotifierConfig
 	}
 	SesNotifierConfig struct {
-		from      string `json:"fromAddress"`
-		secretKey string `json:"secretKey"`
-		accessKey string `json:"accessKey"`
+		EndPoint  string `json:"serverEndpoint"`
+		From      string `json:"fromAddress"`
+		SecretKey string `json:"secretKey"`
+		AccessKey string `json:"accessKey"`
 	}
 )
 
@@ -39,43 +36,33 @@ func (c *SesNotifier) Send(to []string, subject string, msg string) (string, err
 
 	data := make(url.Values)
 	data.Add("Action", "SendEmail")
-	data.Add("Source", c.Config.from)
+	data.Add("Source", c.Config.From)
 	data.Add("Destination.ToAddresses.member.1", strings.Join(to, ", "))
 	data.Add("Message.Subject.Data", subject)
 	data.Add("Message.Body.Text.Data", msg)
-	data.Add("AWSAccessKeyId", c.Config.accessKey)
-
-	log.Printf("to send [%v]", data)
+	data.Add("AWSAccessKeyId", c.Config.AccessKey)
 
 	return c.sesPost(data)
 }
 
-func (c *SesNotifier) authorizationHeader(date string) []string {
-	h := hmac.New(sha256.New, []uint8(c.Config.secretKey))
+func (c *SesNotifier) generateAuthHeader(date string) string {
+	h := hmac.New(sha256.New, []uint8(c.Config.SecretKey))
 	h.Write([]uint8(date))
 	signature := base64.StdEncoding.EncodeToString(h.Sum(nil))
-	auth := fmt.Sprintf("AWS3-HTTPS AWSAccessKeyId=%s, Algorithm=HmacSHA256, Signature=%s", c.Config.accessKey, signature)
-	return []string{auth}
+	return fmt.Sprintf("AWS3-HTTPS AWSAccessKeyId=%s, Algorithm=HmacSHA256, Signature=%s", c.Config.AccessKey, signature)
 }
 
 func (c *SesNotifier) sesPost(data url.Values) (string, error) {
 	body := strings.NewReader(data.Encode())
-	req, err := http.NewRequest("POST", endpoint, body)
+	req, err := http.NewRequest("POST", c.Config.EndPoint, body)
 	if err != nil {
 		return "", err
 	}
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
-	now := time.Now().UTC()
-	// date format: "Tue, 25 May 2010 21:20:27 +0000"
-	date := now.Format("Mon, 02 Jan 2006 15:04:05 -0700")
+	date := time.Now().UTC().Format("Mon, 02 Jan 2006 15:04:05 -0700")
 	req.Header.Set("Date", date)
-
-	h := hmac.New(sha256.New, []uint8(c.Config.secretKey))
-	h.Write([]uint8(date))
-	signature := base64.StdEncoding.EncodeToString(h.Sum(nil))
-	auth := fmt.Sprintf("AWS3-HTTPS AWSAccessKeyId=%s, Algorithm=HmacSHA256, Signature=%s", c.Config.accessKey, signature)
-	req.Header.Set("X-Amzn-Authorization", auth)
+	req.Header.Set("X-Amzn-Authorization", c.generateAuthHeader(date))
 
 	r, err := http.DefaultClient.Do(req)
 	if err != nil {
@@ -88,7 +75,6 @@ func (c *SesNotifier) sesPost(data url.Values) (string, error) {
 
 	if r.StatusCode != 200 {
 		log.Printf("error, status = %d", r.StatusCode)
-
 		log.Printf("error response: %s", resultbody)
 		return "", errors.New(fmt.Sprintf("error code %d. response: %s", r.StatusCode, resultbody))
 	}
