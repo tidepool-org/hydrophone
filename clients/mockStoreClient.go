d797af9fc0b8c4a86cb2f416c049e17b74127433
Jamie <jamie@tidepool.org>
2014-10-31 12:56:04 +1300

    simplify days ago by using time.Now().AddDate()

--- clients/mockStoreClient.go
+++ clients/mockStoreClient.go
@@ -26,29 +26,6 @@ func (d *MockStoreClient) Ping() error {
 	return nil
 }
 
-func dateDaysAgo(date time.Time, daysAgo int) time.Time {
-
-	yr := date.Year()
-	mnt := date.Month()
-	days := date.Day()
-
-	if days > daysAgo {
-		days = days - daysAgo
-	} else if mnt > 1 {
-		//fallback: not enough days
-		mnt = mnt - 1
-		days = 30 - daysAgo
-	} else {
-		//fallback: start of year, go to end of previous yr
-		yr = yr - 1
-		mnt = 12
-		days = 30 - daysAgo
-	}
-
-	return time.Date(yr, mnt, days, 0, 0, 0, 0, time.UTC)
-
-}
-
 func (d *MockStoreClient) UpsertConfirmation(notification *models.Confirmation) error {
 	if d.doBad {
 		return errors.New("UpsertConfirmation failure")
@@ -64,7 +41,7 @@ func (d *MockStoreClient) FindConfirmation(notification *models.Confirmation) (r
 		return nil, nil
 	}
 
-	notification.Created = dateDaysAgo(d.now, 3) // created three days ago
+	notification.Created = time.Now().AddDate(0, 0, -3) // created three days ago
 	return notification, nil
 }
 
@@ -76,7 +53,7 @@ func (d *MockStoreClient) FindConfirmations(confirmation *models.Confirmation, s
 		return nil, nil
 	}
 
-	confirmation.Created = dateDaysAgo(d.now, 3) // created three days ago
+	confirmation.Created = time.Now().AddDate(0, 0, -3) // created three days ago
 	confirmation.Context = []byte(`{"view":{}, "note":{}}`)
 	confirmation.UpdateStatus(statuses[0])
 
