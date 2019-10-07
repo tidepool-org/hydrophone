6517587d1a12baf58b62041cacd8c069cd481328
ydris <ydris@diabeloop.fr>
2019-10-07 16:25:35 +0200

    fine tune the configuration

--- clients/smtpNotifier.go
+++ clients/smtpNotifier.go
@@ -31,13 +31,17 @@ func NewSmtpNotifier(cfg *SmtpNotifierConfig) (*SmtpNotifier, error) {
 // Send a message to a list of recipients with a given subject
 func (c *SmtpNotifier) Send(to []string, subject string, msg string) (int, string) {
 	// Set up authentication information.
-	auth := smtp.PlainAuth("", c.Config.User, c.Config.Password, c.Config.Server)
+	var auth smtp.Auth
+	// If not user is provided, then do not try to authenticate to the server (for dev only)
+	if c.Config.User != "" {
+		auth = smtp.PlainAuth("", c.Config.User, c.Config.Password, c.Config.Server)
+	}
+	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
 	body := []byte("To: " + to[0] + "\r\n" +
 
 		"Subject: " + subject + "\r\n" +
 
-		"\r\n" +
-
+		mime + "\r\n" +
 		msg + "\r\n")
 	err := smtp.SendMail(c.Config.Server, auth, c.Config.From, to, body)
 	if err != nil {
