73a0c59335c45c174ac5b4af1a1df6c012a9e19f
Derrick Burns <derrick@tidepool.org>
2020-01-23 12:22:30 -0800

    Allow injection of null notifier

---
+++ clients/nullNotifier.go
@@ -0,0 +1,23 @@
+package clients
+
+import (
+	"log"
+	"net/smtp"
+	"strings"
+)
+
+type (
+	// NullNotifier
+	NullNotifier struct {
+	}
+)
+
+//NewSmtpNotifier creates a new SMTP notifier (using standard smtp to send emails)
+func NewNullNotifier() (*NullNotifier, error) {
+	return &NullNotifier{}, nil
+}
+
+// Send a message to a list of recipients with a given subject
+func (c *NullNotifier) Send(to []string, subject string, msg string) (int, string) {
+	return 200, "OK"
+}
