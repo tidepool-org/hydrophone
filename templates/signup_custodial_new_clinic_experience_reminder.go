ebfc83fcd404ba5ab176be1563a9d6947a47dc4c
lostlevels <lostlevels@users.noreply.github.com>
2026-03-04 13:24:07 -0800

    Use right copy text.

--- templates/signup_custodial_new_clinic_experience_reminder.go
+++ templates/signup_custodial_new_clinic_experience_reminder.go
@@ -2,7 +2,7 @@ package templates
 
 import "github.com/tidepool-org/hydrophone/models"
 
-const _SignupCustodialNewClinicExperienceReminderSubjectTemplate string = `{{ .ClinicName }} Follow Up - Reminder to Claim your account and get started with Tidepool`
+const _SignupCustodialNewClinicExperienceReminderSubjectTemplate string = `{{ .ClinicName }} Follow Up - Reminder to Claim your Tidepool Account`
 
 func NewSignupCustodialNewClinicExperienceReminderTemplate() (models.Template, error) {
 	return models.NewPrecompiledTemplate(models.TemplateNameSignupCustodialNewClinicExperienceReminder, _SignupCustodialNewClinicExperienceReminderSubjectTemplate, _SignupCustodialNewClinicExperienceBodyTemplate)
