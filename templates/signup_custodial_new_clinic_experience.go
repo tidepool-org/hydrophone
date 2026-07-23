83970e3f987b08969faa876cfe4050f7d842aa50
Todd Kazakov <todd.kazakov@gmail.com>
2026-07-23 11:39:19 +0300

    Update custodial email copy

--- templates/signup_custodial_new_clinic_experience.go
+++ templates/signup_custodial_new_clinic_experience.go
@@ -2,8 +2,9 @@ package templates
 
 import "github.com/tidepool-org/hydrophone/models"
 
-const _SignupCustodialNewClinicExperienceSubjectTemplate string = `Action Required: {{ .ClinicName }} Has Invited You to Tidepool`
+const _SignupCustodialNewClinicExperienceSubjectTemplate string = `{{ .ClinicName }} Follow Up - Action Needed : Follow Up Action Needed`
 const _SignupCustodialNewClinicExperienceBodyTemplate string = `
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
@@ -42,21 +43,33 @@ const _SignupCustodialNewClinicExperienceBodyTemplate string = `
               <table width="100%" style="border-spacing:0;color:#333333;">
                 <tr>
                   <td class="inner centered" style="padding:0;padding:10px;text-align:center;">
-                    <p class="h2 content-width" style="color:#281946;line-height:1.5;Margin:0;Margin-bottom:10px;font-size:14px;font-weight:600;Margin-bottom:28px;Margin-left:auto;Margin-right:auto;max-width:400px;">
-                      {{ .ClinicName }} created a Tidepool account for you, but it looks like you haven't finished setting it up yet.
+                    <a href="{{ .WebURL }}" style="color:#627CFF;text-decoration:none;"><img class="logo" width="220" height="24" src="{{ .AssetURL }}/img/tidepool_logo_light_x2.png" alt="Tidepool logo" style="border:0;display:inline-block;Margin-bottom:32px;max-width:220px;height:auto;"/></a>
+                  </td>
+                </tr>
+                <tr>
+                  <td class="inner centered" style="padding:0;padding:10px;text-align:center;">
+                    <p class="h1 content-width" style="color:#281946;line-height:1.5;Margin:0;font-size:18px;font-weight:700;Margin-bottom:32px;Margin-left:auto;Margin-right:auto;max-width:400px;">
+                      Hi {{ .FullName }},
+                    </p>
+                  </td>
+                </tr>
+                <tr>
+                  <td class="inner centered" style="padding:0;padding:10px;text-align:center;">
+                    <p class="h2 content-width" style="color:#281946;line-height:1.5;Margin:0;font-size:14px;font-weight:400;Margin-left:auto;Margin-right:auto;max-width:400px;">
+                      {{ .ClinicName }} has set up a Tidepool account for you.
                     </p>
                   </td>
                 </tr>
 				<tr>
                   <td class="inner centered" style="padding:0;padding:10px;text-align:center;">
-                    <p class="h2 content-width" style="color:#281946;line-height:1.5;Margin:0;Margin-bottom:10px;font-size:14px;font-weight:600;Margin-bottom:28px;Margin-left:auto;Margin-right:auto;max-width:400px;">
+                    <p class="h2 content-width" style="color:#281946;line-height:1.5;Margin:0;font-size:14px;font-weight:400;Margin-left:auto;Margin-right:auto;max-width:400px;">
                       Tidepool provides secure data storage and intuitive data visualization of your diabetes device data for you, and your care team.
                     </p>
                   </td>
                 </tr>
 				<tr>
                   <td class="inner centered" style="padding:0;padding:10px;text-align:center;">
-                    <p class="h2 content-width" style="color:#281946;line-height:1.5;Margin:0;Margin-bottom:10px;font-size:14px;font-weight:600;Margin-bottom:28px;Margin-left:auto;Margin-right:auto;max-width:400px;">
+                    <p class="h2 content-width" style="color:#281946;line-height:1.5;Margin:0;font-size:14px;font-weight:400;Margin-bottom:28px;Margin-left:auto;Margin-right:auto;max-width:400px;">
                       Click the button below to securely share your diabetes device data with your care team and finish setting up your account.
                     </p>
                   </td>
@@ -64,11 +77,11 @@ const _SignupCustodialNewClinicExperienceBodyTemplate string = `
                 <tr>
                   <td class="inner centered" style="padding:0;padding:10px;text-align:center;">
                     <!--[if (gte mso 9)|(IE)]>
-                    <table bgcolor="#627CFF">
+                    <table bgcolor="#627CFF" width="400" cellpadding="0" cellspacing="0" border="0" align="center">
                     <tr>
-                    <td>
+                    <td align="center">
                     <![endif]-->
-                    <a class="btn primary" href="{{ .WebURL }}/login?signupEmail={{ .Email }}&signupKey={{ .Key }}{{ if .restrictedTokenID }}&restrictedTokenId={{ .restrictedTokenID }}{{ end }}" style="color:#627CFF;text-decoration:none;border-radius:4px;font-size:14px;font-weight:bold;padding:10px 20px;display:inline-block;background-color:#627CFF;color:#FFFFFF;Margin-left:5px;Margin-right:5px;">
+                    <a class="btn primary" href="{{ .WebURL }}/login?signupEmail={{ .Email }}&signupKey={{ .Key }}{{ if .restrictedTokenID }}&restrictedTokenId={{ .restrictedTokenID }}{{ end }}" style="text-decoration:none;border-radius:4px;font-size:14px;font-weight:bold;padding:13px 20px;display:block;background-color:#627CFF;color:#FFFFFF;max-width:360px;Margin-left:auto;Margin-right:auto;text-align:center;">
                       Set Up Your Tidepool Account
                     </a>
                     <!--[if (gte mso 9)|(IE)]>
@@ -80,12 +93,15 @@ const _SignupCustodialNewClinicExperienceBodyTemplate string = `
                 </tr>
                 <tr>
                   <td class="inner centered" style="padding:0;padding:10px;text-align:center;">
-                    <p style="color:#281946;font-size:14px;line-height:1.5;font-weight:600;Margin-bottom:10px;Margin-left:auto;Margin-right:auto;max-width:400px;">If you have any questions, don’t hesitate to email <a href="mailto:support@tidepool.org">support@tidepool.org</a>. The Tidepool support team is available to help.</p>
+                    <p style="color:#281946;font-size:14px;line-height:1.5;font-weight:400;Margin:0;Margin-top:18px;Margin-bottom:10px;Margin-left:auto;Margin-right:auto;max-width:400px;">If you have any questions about this message or Tidepool, please contact <a href="mailto:support@tidepool.org" style="color:#627CFF;text-decoration:underline;">support@tidepool.org</a> or <a href="https://support.tidepool.org" style="color:#627CFF;text-decoration:underline;">learn more</a> here.</p>
                   </td>
                 </tr>
                 <tr>
                   <td class="inner centered" style="padding:0;padding:10px;text-align:center;">
-                    <a href="{{ .WebURL }}" style="color:#627CFF;text-decoration:none;"><img class="logo" width="220" height="24" src="{{ .AssetURL }}/img/tidepool_logo_light_x2.png" alt="Tidepool logo" style="border:0;display:inline-block;Margin-bottom:36px;max-width:220px;height:auto;"/></a>
+                    <p class="content-width" style="color:#281946;font-size:14px;line-height:1.5;font-weight:400;Margin:0;Margin-bottom:28px;Margin-left:auto;Margin-right:auto;max-width:400px;">
+                      Sincerely,<br/>
+                      The Tidepool Team
+                    </p>
                   </td>
                 </tr>
                 <tr>
@@ -108,7 +124,7 @@ const _SignupCustodialNewClinicExperienceBodyTemplate string = `
                 </tr>
                 <tr>
                   <td class="inner centered" style="padding:0;padding:10px;text-align:center;">
-                    <p class="about content-width narrow" style="color:#281946;font-size:14px;line-height:1.5;font-weight:600;Margin:0;Margin-bottom:10px;font-size:10px;font-weight:300;color:#6d6d6d;Margin-bottom:0;max-width:400px;Margin-left:auto;Margin-right:auto;max-width:350px;">
+                    <p class="about content-width narrow" style="color:#6d6d6d;line-height:1.5;Margin:0;font-size:10px;font-weight:300;Margin-bottom:0;Margin-left:auto;Margin-right:auto;max-width:350px;">
                       <a href="https://www.tidepool.org" style="color:#627CFF;text-decoration:none;">Tidepool</a>
                       An open source, not-for-profit effort to build an open data platform and better applications that reduce the burden of diabetes.
                     </p>
@@ -124,7 +140,7 @@ const _SignupCustodialNewClinicExperienceBodyTemplate string = `
                           <tr>
                           <td>
                           <![endif]-->
-                          <a class="btn secondary small" href="http://support.tidepool.org" style="color:#627CFF;text-decoration:none;border-radius:4px;font-size:14px;font-weight:bold;padding:10px 20px;display:inline-block;border:1px solid #dbdee0;background-color:#FFFFFF;color:#281946;font-weight:normal;padding:4px 10px 5px;Margin-left:3px;Margin-right:3px;font-size:10px;border-radius:2px;">
+                          <a class="btn secondary small" href="https://support.tidepool.org" style="text-decoration:none;border-radius:2px;font-size:10px;font-weight:normal;padding:4px 10px 5px;display:inline-block;border:1px solid #dbdee0;background-color:#FFFFFF;color:#281946;Margin-left:3px;Margin-right:3px;">
                             Get Support
                           </a>
                           <!--[if (gte mso 9)|(IE)]>
@@ -150,6 +166,7 @@ const _SignupCustodialNewClinicExperienceBodyTemplate string = `
     </center>
   </body>
 </html>
+
 `
 
 func NewSignupCustodialNewClinicExperienceTemplate() (models.Template, error) {
--- templates/signup_custodial_new_clinic_experience_reminder.go
+++ templates/signup_custodial_new_clinic_experience_reminder.go
@@ -2,7 +2,7 @@ package templates
 
 import "github.com/tidepool-org/hydrophone/models"
 
-const _SignupCustodialNewClinicExperienceReminderSubjectTemplate string = `Reminder: Accept Your Tidepool Invitation from {{ .ClinicName }}`
+const _SignupCustodialNewClinicExperienceReminderSubjectTemplate string = `Reminder: Accept Your Invitation from {{ .ClinicName }} to Share Your Diabetes Data`
 
 func NewSignupCustodialNewClinicExperienceReminderTemplate() (models.Template, error) {
 	return models.NewPrecompiledTemplate(models.TemplateNameSignupCustodialNewClinicExperienceReminder, _SignupCustodialNewClinicExperienceReminderSubjectTemplate, _SignupCustodialNewClinicExperienceBodyTemplate)
